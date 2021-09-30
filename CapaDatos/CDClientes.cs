4603dd25c24629b3ac6bd5af733342db95bfd154
alex_ <alex_@DESKTOP-3L7GU89>
2021-09-30 09:30:41 -0500

    Agregar archivos de proyecto.

---
+++ CapaDatos/CDClientes.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using CapaDatos;
+using System.Data;
+
+
+namespace CapaDatos
+{
+    public class CDClientes
+
+    {
+
+        string CNX = "Server=185.151.28.144; User= amunoz; Password=@lexander10; Port=57220; database=RescatesDB-31393698f0; SSL Mode= 0";
+
+
+            public void PruebaConexion()
+        {
+
+            MySqlConnection conexion = new MySqlConnection(CNX);
+
+            try
+            {
+                conexion.Open();
+            
+
+
+            }
+            catch (Exception ex)
+            {
+
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+
+                
+                
+                
+                
+                
+                
+                return;
+
+            }
+            System.Windows.Forms.MessageBox.Show("Conectado");
+
+        }
+
+
+        public void pruebamysql()
+        { 
+        
+
+        }
+
+    }
+}
---
+++ CapaPresentacion/FormUnidades.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CapaDatos;
+using MySql.Data.MySqlClient;
+
+namespace CapaPresentacion
+{
+
+
+    public partial class FormUnidades : Form
+    {
+        public FormUnidades()
+        {
+            InitializeComponent();
+        }
+
+        private void FormUnidades_Load(object sender, EventArgs e)
+        {
+            prueba();
+
+      
+
+
+        }
+
+        public void prueba()
+        {
+
+            CapaDatos.CDClientes cDClientes = new CDClientes();
+
+            cDClientes.PruebaConexion();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            prueba();
+        }
+    }
+}
